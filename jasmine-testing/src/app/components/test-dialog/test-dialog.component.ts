4f03974e738840f09b0c65ff543e36f61137e25b
Brandon <99165912+bdebruyn02@users.noreply.github.com>
2024-05-28 12:43:46 +0200

    add test dialog

---
+++ jasmine-testing/src/app/components/test-dialog/test-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './test-dialog.component.html',
+  styleUrl: './test-dialog.component.scss'
+})
+export class TestDialogComponent {
+
+}
